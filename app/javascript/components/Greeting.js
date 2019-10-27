a94068db19e4bfdc90cfdcfdbde5ae37720f0d90
Timo Mämecke <timo@maemecke.com>
2019-10-27 22:30:23 +0100

    Revert test text change

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Greeting = ({ name }) => {
-  return <Title>Hellooo {name}!</Title>
+  return <Title>Hello {name}!</Title>
 }
 
 Greeting.propTypes = {
