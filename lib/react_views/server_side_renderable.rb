d80cbc06d3ff580f800af2852d128265b8956577
Timo Mämecke <timo@maemecke.com>
2019-10-27 22:28:51 +0100

    Fix __react_views_head attr in concern

--- lib/react_views/server_side_renderable.rb
+++ lib/react_views/server_side_renderable.rb
@@ -3,7 +3,7 @@ module ReactViews
     extend ActiveSupport::Concern
 
     included do
-      attr_accessor :__react_views_ssr_head
+      attr_accessor :__react_views_head
       attr_accessor :__react_views_props
       attr_accessor :__react_views_view_name
     end
